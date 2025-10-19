c6aea0d09791c9062f318213bfc74aafe82e11e5
thomashorsey <thomasdhorsey@gmail.com>
2025-10-19 19:39:19 +1000

    Make pieces piece shaped, Pull apart long methods, Check valid placement to snap

--- script.js
+++ script.js
@@ -1,14 +1,72 @@
-const numberOfPieces = 48;
+const ROWS = 6;
+const COLUMNS = 8;
+const PIECE_SIZE = 50;
+const MASK_SIZE_MULTIPLIER = 1 / 1; // 0.75 is the correct scale
+const NUMBER_OF_PIECES = ROWS * COLUMNS;
+const BOARD_HEIGHT = ROWS * PIECE_SIZE;
+const BOARD_WIDTH = COLUMNS * PIECE_SIZE;
+const MARGIN = PIECE_SIZE / 2;
+
+const SIDE = {
+    TOP: 0,
+    RIGHT: 1,
+    BOTTOM: 2,
+    LEFT: 3,
+}
+
+const SIDE_OFFSET_HORIZONTAL = {
+    0: 0,
+    1: PIECE_SIZE,
+    2: 0,
+    3: -PIECE_SIZE
+}
+
+const SIDE_OFFSET_VERTICAL = {
+    0: -PIECE_SIZE,
+    1: 0,
+    2: PIECE_SIZE,
+    3: 0
+}
+
+const SIDE_TYPE = {
+    FLAT: 'FLAT',
+    TAB: 'TAB',
+    BLANK: 'BLANK'
+}
+
+const COMPATIBLE_SIDE_TYPE = {
+    FLAT: SIDE_TYPE.FLAT,
+    TAB: SIDE_TYPE.BLANK,
+    BLANK: SIDE_TYPE.TAB
+}
+
+const PIECE_TYPE_SIDES = {
+    tl: [SIDE_TYPE.FLAT, SIDE_TYPE.BLANK, SIDE_TYPE.TAB, SIDE_TYPE.FLAT],
+    tr: [SIDE_TYPE.FLAT, SIDE_TYPE.FLAT, SIDE_TYPE.TAB, SIDE_TYPE.TAB],
+    t: [SIDE_TYPE.FLAT, SIDE_TYPE.BLANK, SIDE_TYPE.TAB, SIDE_TYPE.TAB],
+    bl: [SIDE_TYPE.BLANK, SIDE_TYPE.BLANK, SIDE_TYPE.FLAT, SIDE_TYPE.FLAT],
+    br: [SIDE_TYPE.BLANK, SIDE_TYPE.FLAT, SIDE_TYPE.FLAT, SIDE_TYPE.TAB],
+    b: [SIDE_TYPE.BLANK, SIDE_TYPE.BLANK, SIDE_TYPE.FLAT, SIDE_TYPE.TAB],
+    ml: [SIDE_TYPE.BLANK, SIDE_TYPE.BLANK, SIDE_TYPE.TAB, SIDE_TYPE.FLAT],
+    mr: [SIDE_TYPE.BLANK, SIDE_TYPE.FLAT, SIDE_TYPE.TAB, SIDE_TYPE.TAB],
+    m: [SIDE_TYPE.BLANK, SIDE_TYPE.BLANK, SIDE_TYPE.TAB, SIDE_TYPE.TAB]
+}
+
 let highestZIndex = 1;
 let activePiece = null;
 
-// Wait for DOM to be fully loaded before accessing its elements
 document.addEventListener("DOMContentLoaded", () => {
     const instructions = document.getElementById("instructions");
     const showInstructionsButton = document.getElementById("showInstructions");
     const hideInstructionsButton = document.getElementById("hideInstructions");
 
-    createAndPlacePieces(numberOfPieces);
+    board.style.height = `${BOARD_HEIGHT}px`;
+    board.style.width = `${BOARD_WIDTH}px`;
+
+    instructions.style.height = `${BOARD_HEIGHT}px`;
+    instructions.style.width = `${BOARD_WIDTH}px`;
+
+    createAndPlacePieces(NUMBER_OF_PIECES);
 
     showInstructionsButton.addEventListener('click', () => {
         instructions.style.display = "block";
@@ -28,14 +86,61 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
+function getPieceType(row, column) {
+    const isTop = row === 0;
+    const isBottom = row === ROWS - 1;
+    const isLeft =  column === 0;
+    const isRight = column === COLUMNS - 1;
+
+    if (isTop && isLeft) return 'tl';
+    if (isTop && isRight) return 'tr';
+    if (isTop) return 't';
+
+    if (isBottom && isLeft) return 'bl';
+    if (isBottom && isRight) return'br';
+    if (isBottom) return 'b';
+
+    if (isLeft) return 'ml';
+    if (isRight) return 'mr';
+    return 'm';
+}
+
+function getRandomNonOverlappingPosition(mainRect, boardRect, pieceSize) {
+    let randomTop, randomLeft;
+    let isOverlapping = true;
+    
+    const maxTop = mainRect.height - MARGIN - pieceSize;
+    const maxLeft = mainRect.width - MARGIN - pieceSize;
+
+    while (isOverlapping) {
+        randomTop = Math.random() * maxTop;
+        randomLeft = Math.random() * maxLeft;
+
+        const pieceTop = mainRect.top + randomTop;
+        const pieceLeft = mainRect.left + randomLeft;
+        const pieceBottom = pieceTop + pieceSize;
+        const pieceRight = pieceLeft + pieceSize;
+
+        const boardCollisionLeft = boardRect.left - MARGIN;
+        const boardCollisionRight = boardRect.right + MARGIN;
+        const boardCollisionTop = boardRect.top - MARGIN;
+        const boardCollisionBottom = boardRect.bottom + MARGIN;
+
+        if (
+            pieceRight < boardCollisionLeft ||
+            pieceLeft > boardCollisionRight ||
+            pieceBottom < boardCollisionTop ||
+            pieceTop > boardCollisionBottom
+        ) {
+            isOverlapping = false;
+        }
+    }
+    return { randomTop, randomLeft };
+}
+
 function createAndPlacePieces(count) {
     const main = document.querySelector("main");
     const board = document.getElementById("board");
-    const mainMargin = 20;
-    const boardMargin = 20;
-
-    const pieceHeight = 40;
-    const pieceWidth = 40;
 
     const mainRect = main.getBoundingClientRect();
     const boardRect = board.getBoundingClientRect();
@@ -44,43 +149,25 @@ function createAndPlacePieces(count) {
         const piece = document.createElement("div");
         piece.classList.add("piece");
 
-        // Set to the new highest z-index
+        piece.style.height = `${Math.ceil(PIECE_SIZE * MASK_SIZE_MULTIPLIER)}px`;
+        piece.style.width = `${Math.ceil(PIECE_SIZE * MASK_SIZE_MULTIPLIER)}px`;
+
+        const row = Math.floor(i / COLUMNS);
+        const column = i % COLUMNS;
+        const pieceType = getPieceType(row, column)
+        piece.dataset.pieceType = pieceType;
+        piece.style.maskImage = `url(#${pieceType})`;
+
         highestZIndex++;
         piece.style.zIndex = highestZIndex;
 
-        // Generate a random rotation for each piece
         const randomRotation = Math.floor(Math.random() * 360);
         piece.dataset.rotation = randomRotation;
         piece.style.transform = `rotate(${randomRotation}deg)`;
 
-        let randomTop, randomLeft;
-        let isOverlapping = true;
-
-        // Generating new positions until a valid one is found
-        while (isOverlapping) {
-            randomTop = Math.random() * (mainRect.height - mainMargin - pieceHeight);
-            randomLeft = Math.random() * (mainRect.width - mainMargin - pieceWidth);
-
-            // Calculate piece position relative to main
-            const pieceTop = mainRect.top + randomTop;
-            const pieceLeft = mainRect.left + randomLeft;
-            const pieceBottom = pieceTop + pieceHeight;
-            const pieceRight = pieceLeft + pieceWidth;
-
-            // Check for overlap with the board or its margin
-            if (
-                pieceRight < boardRect.left - boardMargin ||
-                pieceLeft > boardRect.right + boardMargin ||
-                pieceBottom < boardRect.top - boardMargin ||
-                pieceTop > boardRect.bottom + boardMargin
-            ) {
-                isOverlapping = false;
-            }
-        }
-
-        // Set position relative to main
-        piece.style.top = (mainRect.top + randomTop) + 'px';
-        piece.style.left = (mainRect.left + randomLeft) + 'px';
+        const { randomTop, randomLeft } = getRandomNonOverlappingPosition(mainRect, boardRect, PIECE_SIZE);
+        piece.style.top = `${mainRect.top + randomTop}px`;
+        piece.style.left = `${mainRect.left + randomLeft}px`;
 
         main.appendChild(piece);
         piece.addEventListener('click', togglePiece);
@@ -91,19 +178,15 @@ function togglePiece(e) {
     e.preventDefault();
     const clickedPiece = e.target;
 
-    // Detect if we are picking up or putting down the piece
     if (activePiece === null) {
         activePiece = clickedPiece;
 
-        // Set to the new highest z-index
         highestZIndex++;
         activePiece.style.zIndex = highestZIndex;
 
-        // Update cursor
         activePiece.style.cursor = 'grabbing';
         document.addEventListener('mousemove', moveActivePiece);
 
-        // Snap rotation to the nearest 90 degrees
         let currentRotation = parseInt(activePiece.dataset.rotation) || 0;
         const snappedRotation = Math.round(currentRotation / 90) * 90;
         activePiece.dataset.rotation = snappedRotation;
@@ -117,74 +200,136 @@ function moveActivePiece(e) {
     if (activePiece) {
         const newTop = e.clientY - (activePiece.offsetHeight / 2);
         const newLeft = e.clientX - (activePiece.offsetWidth / 2);
-        activePiece.style.top = newTop + 'px';
-        activePiece.style.left = newLeft + 'px';
+
+        activePiece.style.top = `${newTop}px`;
+        activePiece.style.left = `${newLeft}px`;
     }
 }
 
 function putDownPiece() {
     if (activePiece) {
-        // Update cursor
         document.removeEventListener('mousemove', moveActivePiece);
         activePiece.style.cursor = 'grab';
 
-        // Snap piece
         snapToGrid(activePiece);
 
-        // Reset selection
         activePiece = null;
     }
 }
 
-function snapToGrid(pieceEl) {
-    const board = document.getElementById("board");
-    const gridSize = 50;
-    const boardRect = board.getBoundingClientRect();
-    const pieceRect = pieceEl.getBoundingClientRect();
-
-    // Calculate overlap
+function isPieceOverTheBoard(pieceRect, boardRect) {
     const overlapX = Math.max(0, Math.min(pieceRect.right, boardRect.right) - Math.max(pieceRect.left, boardRect.left));
     const overlapY = Math.max(0, Math.min(pieceRect.bottom, boardRect.bottom) - Math.max(pieceRect.top, boardRect.top));
 
-    // Check overlap is greater than 50%
-    const isMoreThanFiftyPercentX = overlapX / pieceRect.width > 0.5;
-    const isMoreThanFiftyPercentY = overlapY / pieceRect.height > 0.5;
+    const isOverlapMoreThanHalfX = overlapX / pieceRect.width > 0.5;
+    const isOverlapMoreThanHalfY = overlapY / pieceRect.height > 0.5;
 
-    if (isMoreThanFiftyPercentX && isMoreThanFiftyPercentY) {
-        // Calculate snapped position relative to board
-        let snappedX = Math.round((pieceRect.left - boardRect.left) / gridSize) * gridSize;
-        let snappedY = Math.round((pieceRect.top - boardRect.top) / gridSize) * gridSize;
+    return isOverlapMoreThanHalfX && isOverlapMoreThanHalfY;
+}
 
-        // Clamp snapped position within board
-        const maxPieceLeft = boardRect.width - pieceEl.offsetWidth;
-        const maxPieceTop = boardRect.height - pieceEl.offsetHeight;
-        snappedX = Math.max(0, Math.min(snappedX, maxPieceLeft));
-        snappedY = Math.max(0, Math.min(snappedY, maxPieceTop));
+function isPositionOffTheBoard(targetLeft, targetTop, boardRect) {
+    return (
+        targetTop < boardRect.top ||
+        targetLeft + PIECE_SIZE > boardRect.right ||
+        targetTop + PIECE_SIZE > boardRect.bottom ||
+        targetLeft < boardRect.left
+    );
+}
 
-        // Calculate target position
-        const targetLeft = boardRect.left + snappedX;
-        const targetTop= boardRect.top + snappedY;
+function getPieceInPosition(targetLeft, targetTop) {
+    const pieces = document.querySelectorAll(".piece");
+    for (const piece of pieces) {
+        const pieceRect = piece.getBoundingClientRect();
+        if (pieceRect.left === targetLeft && pieceRect.top === targetTop) {
+            return piece;
+        }
+    }
+    return undefined;
+}
 
-        // Check if target position is occupied
-        let isPositionOccupied = false;
-        const allPieces = document.querySelectorAll(".piece");
-        for (const otherPiece of allPieces) {
-            if (otherPiece === pieceEl) {
-                continue;
+function getPieceSideType(piece, side) {
+    const rotationOffset = (piece.dataset.rotation / 90) % 360;
+    const pieceSideTypes = PIECE_TYPE_SIDES[piece.dataset.pieceType];
+
+    return pieceSideTypes.at(side - rotationOffset);
+}
+
+function isPositionValid(piece, targetLeft, targetTop, boardRect) {
+    const sides = Object.values(SIDE);
+    for (const side of sides) {
+        const pieceSideType = getPieceSideType(piece, side);
+
+        const neighborLeft = targetLeft + SIDE_OFFSET_HORIZONTAL[side];
+        const neighborTop = targetTop + SIDE_OFFSET_VERTICAL[side];
+
+        if (isPositionOffTheBoard(neighborLeft, neighborTop, boardRect)) {
+            if (SIDE_TYPE.FLAT != COMPATIBLE_SIDE_TYPE[pieceSideType]) {
+                return false;
             }
-
-            // Compare current piece position with target position
-            const otherPieceRect = otherPiece.getBoundingClientRect();
-            if (otherPieceRect.left === targetLeft && otherPieceRect.top === targetTop) {
-                isPositionOccupied = true;
-                break;
-            }
+            continue;
         }
 
-        // Snap into position if its unoccupied
-        if (!isPositionOccupied) {
-            pieceEl.style.left = targetLeft + "px";
-            pieceEl.style.top = targetTop + "px";
+        const neighborPiece = getPieceInPosition(neighborLeft, neighborTop);
+        if (!neighborPiece) {
+            if (SIDE_TYPE.FLAT == pieceSideType) {
+                return false;
+            }
+            continue;
         }
+
+        const neighborSideType = getPieceSideType(neighborPiece, (side + 2) % 4);
+        if (neighborSideType != COMPATIBLE_SIDE_TYPE[pieceSideType]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isPositionEmpty(piece, targetLeft, targetTop) {
+    const pieces = document.querySelectorAll(".piece");
+    for (const otherPiece of pieces) {
+        if (otherPiece === piece) {
+            continue;
+        }
+
+        const otherPieceRect = otherPiece.getBoundingClientRect();
+        if (otherPieceRect.left === targetLeft && otherPieceRect.top === targetTop) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function snapToGrid(piece) {
+    const board = document.getElementById("board");
+
+    const boardRect = board.getBoundingClientRect();
+    const pieceRect = piece.getBoundingClientRect();
+
+    if (!isPieceOverTheBoard(pieceRect, boardRect)) {
+        return;
+    }
+
+    // TODO correct gaps from piece scaling (this will be a rabbit hole)
+
+    let snappedX = Math.round((pieceRect.left - boardRect.left) / PIECE_SIZE) * PIECE_SIZE;
+    let snappedY = Math.round((pieceRect.top - boardRect.top) / PIECE_SIZE) * PIECE_SIZE;
+
+    const maxPieceLeft = boardRect.width - piece.offsetWidth;
+    const maxPieceTop = boardRect.height - piece.offsetHeight;
+
+    snappedX = Math.max(0, Math.min(snappedX, maxPieceLeft));
+    snappedY = Math.max(0, Math.min(snappedY, maxPieceTop));
+
+    const targetLeft = boardRect.left + snappedX;
+    const targetTop = boardRect.top + snappedY;
+
+    if (!isPositionEmpty(piece, targetLeft, targetTop)) {
+        return;
+    }
+
+    if (isPositionValid(piece, targetLeft, targetTop, boardRect)) {
+        piece.style.left = `${targetLeft}px`;
+        piece.style.top = `${targetTop}px`;
     }
 }
